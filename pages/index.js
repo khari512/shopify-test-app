26146b27cf5f9478955612f811d9ff17ad2dbc0c
hkolli <hkolli@ftdi.com>
2021-04-20 10:43:34 +0530

    compile error in index js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { get  } from 'lodash';
 import { TextStyle, EmptyState, Layout, Page, Card, DataTable} from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 import store from 'store-js';
-import ResourceListWithProducts from '../components/ResourceList';
+//import ResourceListWithProducts from '../components/ResourceList';
 import axios from "axios";
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
